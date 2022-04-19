931daffbbcdc4114d1eff68ef55876c72bc2dcc7
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-20 00:13:39 +0430

    routing implimented in App component-fix2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        {routes.map((rout) => {
-         return <Route path={rout.path} element={rout.element} key={rout.id} />;
-        })}
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((rout) => {
+            return (
+              <Route path={rout.path} element={rout.element} key={rout.id} />
+            );
+          })}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
