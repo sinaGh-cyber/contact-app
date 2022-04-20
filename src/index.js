94600fa645878471dfa2d8b25714f33945aa4c5c
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-20 16:40:04 +0430

    browserRouter warped app in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ContactProvider from './context/contactProvider/contactProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContactProvider>
-      <App />
-    </ContactProvider>
+    <BrowserRouter>
+      <ContactProvider>
+        <App />
+      </ContactProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
