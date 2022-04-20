a3cae3bf074eb89174e96cb8eb3911462e200ed7
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-20 16:38:31 +0430

    root element in App changed + clean-up unnessecry imports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import './App.css';
 import routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {routes.map((rout) => {
-            return (
-              <Route path={rout.path} element={rout.element} key={rout.id} />
-            );
-          })}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <main className="App">
+      <Routes>
+        {routes.map((rout) => {
+          return (
+            <Route path={rout.path} element={rout.element} key={rout.id} />
+          );
+        })}
+      </Routes>
+    </main>
   );
 }
 
