1c2e560637c4b545e62cad0c233a24f0a1f8840d
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-20 19:49:02 +0430

    TodastContainer added to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import routes from './routes';
 
 function App() {
   return (
     <main className="App">
+      <ToastContainer theme='colored' />
       <Routes>
         {routes.map((rout) => {
           return (
