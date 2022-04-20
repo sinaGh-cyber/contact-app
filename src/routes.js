e4251934dac0ba96eb0567d806412eb043959ac7
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-20 19:46:39 +0430

    new rout added to rote.js

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
+import AddContact from './components/AddContact/AddContact';
 import EditContact from './Pages/EditContact/EditContact';
 import HomePage from './Pages/HomePage/HomePage';
 import NotFound from './Pages/NotFound/NotFound';
 
 const routes = [
-  { path: '/edit/:id', element: <EditContact />, id: 1 },
-  { path: '/', element: <HomePage />, id: 0 },
+  { path: '/edit/:id', element: <EditContact />, id: 0 },
+  { path: '/add', element: <AddContact />, id: 1 },
+  { path: '/', element: <HomePage />, id: 2 },
   { path: '*', element: <NotFound />, id: 3 },
 ];
 export default routes;
