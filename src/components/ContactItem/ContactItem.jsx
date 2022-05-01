1fbcc197b00bbc339411692712a3a5923e783829
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-01 22:25:11 +0430

    ContactItem component created and styled

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,16 +2,23 @@ import styles from './ContactItem.module.scss';
 import { AiFillDelete } from 'react-icons/ai';
 import { GrEdit } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
+import { useContactDispatcher } from '../../context/contactProvider/contactProvider';
 const ContactItem = ({ contact }) => {
+  const dispatch = useContactDispatcher();
+
+  const deleteHandler = () => {
+    dispatch({ type: 'deleteContact', id: contact.id });
+  };
   return (
     <li className={styles.liTag}>
       <div className={styles.info}>
-        <p className={styles.email}>{contact.email}</p>
+        {' '}
         <p className={styles.name}>{contact.name}</p>
+        <p className={styles.email}>{contact.email}</p>
       </div>
       <div className={styles.buttons}>
         {' '}
-        <button className={styles.deleteBtn}>
+        <button onClick={deleteHandler} className={styles.deleteBtn}>
           <AiFillDelete />
         </button>
         <Link to={`/edit/${contact.id}/`}>
