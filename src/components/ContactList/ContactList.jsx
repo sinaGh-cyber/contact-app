eb685b60435b3ede77f33a6db94ee45aefc48446
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-04 00:32:40 +0430

    Loader added to ContactList component + some more styling

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,19 @@
 import styles from './ContactList.module.scss';
-import {
-  useContact
-} from '../../context/contactProvider/contactProvider';
+import { useContact } from '../../context/contactProvider/contactProvider';
 import ContactItem from '../ContactItem/ContactItem';
+import Loader from '../Loader/Loader';
 
 const ContactList = () => {
   const contacts = useContact();
   return (
-    <ul className={styles.ulTag}>
+    <ul className={`${styles.ulTag} ${contacts.currentStatus !== 'Loaded' && styles.loading}`}>
       {contacts.currentStatus === 'Loaded' ? (
-        
-          contacts.filteredContacts.map((contact) => {
-            return <ContactItem contact={contact} key={contact.id} />;
-          })
-        
-      ): 'Loading...'}
+        contacts.filteredContacts.map((contact) => {
+          return <ContactItem contact={contact} key={contact.id} />;
+        })
+      ) : (
+        <Loader />
+      )}
     </ul>
   );
 };
