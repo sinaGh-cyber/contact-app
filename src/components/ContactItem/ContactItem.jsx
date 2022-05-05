bfb2430640a86ef638be900e0994d2315ea1f116
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-05 22:55:44 +0430

    Selecting functionality for each item implimented

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,9 +2,21 @@ import styles from './ContactItem.module.scss';
 import { AiFillDelete } from 'react-icons/ai';
 import { GrEdit } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
-import { useContactDispatcher } from '../../context/contactProvider/contactProvider';
+import { GrCheckboxSelected, GrCheckbox } from 'react-icons/gr';
+import {
+  useContact,
+  useContactDispatcher,
+} from '../../context/contactProvider/contactProvider';
 const ContactItem = ({ contact }) => {
   const dispatch = useContactDispatcher();
+  const { isSelectModeOn } = useContact();
+
+  const selectItemHandler = () => {
+    dispatch({
+      type: 'toggleContactItemSelectionStatus',
+      id: contact.id,
+    });
+  };
 
   const deleteHandler = () => {
     dispatch({ type: 'deleteContact', id: contact.id });
@@ -17,15 +29,22 @@ const ContactItem = ({ contact }) => {
         <p className={styles.email}>{contact.email}</p>
       </div>
       <div className={styles.buttons}>
-        {' '}
-        <button onClick={deleteHandler} className={styles.deleteBtn}>
-          <AiFillDelete />
-        </button>
-        <Link to={`/edit/${contact.id}/`}>
-          <button className={styles.editBtn}>
-            <GrEdit />
+        {isSelectModeOn ? (
+          <button onClick={selectItemHandler}>
+            {contact.isSelected ? <GrCheckboxSelected /> : <GrCheckbox />}
           </button>
-        </Link>
+        ) : (
+          <>
+            <button onClick={deleteHandler} className={styles.deleteBtn}>
+              <AiFillDelete />
+            </button>
+            <Link to={`/edit/${contact.id}/`}>
+              <button className={styles.editBtn}>
+                <GrEdit />
+              </button>
+            </Link>
+          </>
+        )}{' '}
       </div>
     </li>
   );
