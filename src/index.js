dcdd3b05eb458d09686c34c6cd7eab29ffd3b1d4
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-08 02:43:30 +0430

    App component warped by AlertProvider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import ContactProvider from './context/contactProvider/contactProvider';
 import { BrowserRouter } from 'react-router-dom';
+import AlertProvider from './context/AlertProvider/AlertProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ContactProvider>
-        <App />
-      </ContactProvider>
+      <AlertProvider>
+        <ContactProvider>
+          <App />
+        </ContactProvider>
+      </AlertProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
