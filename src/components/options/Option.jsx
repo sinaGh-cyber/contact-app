c5b7e086c4ee6dfda6fbe329e175d6eaa133614b
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-22 23:46:50 +0430

    Option component jsx ready

--- src/components/options/Option.jsx
+++ src/components/options/Option.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import styles from './Option.module.scss';
+import { BsThreeDots } from 'react-icons/bs';
 const Option = () => {
-    return ( <div className={styles.BtnContainer} ></div> );
-}
- 
+  const [isOptionsActive, setIsOptionsActive] = useState();
+  return (
+    <div className={styles.BtnContainer}>
+      {isOptionsActive ? (
+        <button>
+          <BsThreeDots />
+        </button>
+      ) : (
+        <menu>
+            <li><button>Delete</button></li>
+            <li><button>Edit</button></li>
+        </menu>
+      )}
+    </div>
+  );
+};
+
 export default Option;
