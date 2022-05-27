7765188814107946a1958f38287e00f36ad37442
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-27 23:50:18 +0430

    OptionProvider initStat updated

--- src/context/OptionProvider/initStat.js
+++ src/context/OptionProvider/initStat.js
@@ -1,9 +1,10 @@
 const initStat = {
-    isOptionMenuVisible: false,
-    onDelete: () => {
-      throw Error("onDelete call back function doesn't passed");
-    },
-    elemRef: Error("elementReference doesn't passed "),
-  };
+  isOptionMenuVisible: false,
+  onDelete: () => {
+    throw Error("onDelete call back function doesn't passed");
+  },
+  elemRef: Error("elementReference doesn't passed "),
+  id: NaN,
+};
 
-  export default initStat;
+export default initStat;
