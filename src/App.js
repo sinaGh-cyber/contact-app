e8d17993a14a786a44f3e1a9c3cdab160e6fc109
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-05-29 23:59:25 +0430

    making option menu unvisible after click on screen handeld

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import './App.scss';
 import Alert from './components/Alert/Alert';
+import { useOption } from './context/OptionProvider/OptionProvider';
 import routes from './routes';
 
 function App() {
+  const { contactOptionMenu, dispatchOption } = useOption();
   return (
-    <div className="App">
+    <div
+      onClick={(e) => {
+        dispatchOption({ type: 'optionClose' });
+      }}
+      className="App"
+    >
       <Alert />
+
       <section className="container">
         <ToastContainer
           theme="colored"
